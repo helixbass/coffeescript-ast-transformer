be3d85a6b6a12fcf84e83cba81f41bf775c9366a
Julian Rosse <julian@helixbass.net>
2021-03-07 12:24:41 -0500

    emit simple variable declaration

--- run.js
+++ run.js
@@ -4,15 +4,31 @@ const fs = require('fs')
 const util = require('util')
 const coffeescript = require('coffeescript')
 const {default: generate} = require('@babel/generator')
+const {omit, mapValues, flow, map} = require('lodash/fp')
+const {isPlainObject, isArray} = require('lodash')
 
 const {getTransformedAst} = require('./lib')
 
 const [,, command] = process.argv
 
+const stripLocationDataAndTokens = (obj) =>
+  isArray(obj)
+    ? map(stripLocationDataAndTokens)(obj)
+    : isPlainObject(obj)
+    ? flow(
+        omit(['tokens', 'loc', 'range', 'start', 'end']),
+        mapValues(stripLocationDataAndTokens)
+      )(obj)
+    : obj
+
 const dump = (obj) => console.log(util.inspect(obj, false, null))
 
 const inputSource = fs.readFileSync('./tmp.coffee', 'utf-8')
 const inputAst = coffeescript.compile(inputSource, {ast: true})
+if (command === 'coffee-ast') {
+  dump(stripLocationDataAndTokens(inputAst))
+  return
+}
 const transformedAst = getTransformedAst(inputAst)
 if (command === 'js') {
   const {code: printed} = generate(transformedAst)
--- src/transform-plugin.coffee
+++ src/transform-plugin.coffee
@@ -13,9 +13,30 @@ withNullReturnValues = mapValues (f) ->
       f ...args
       null
 
+class Scope
+  constructor: ->
+    @declaredVariables = []
+
+  addDeclaredVariable: (name) ->
+    @declaredVariables.push name
+
+  hasDeclaredVariables: ->
+    !!@declaredVariables.length
+
 transformer = ({types: t}) ->
+  addVariableDeclarations = (path, scope) ->
+    path.unshiftContainer 'body',
+      t.variableDeclaration 'var', scope.declaredVariables.map (name) ->
+        t.variableDeclarator t.identifier name
+
   visitor: withNullReturnValues(
-    Program: ->
+    Program:
+      enter: (_, state) ->
+        state.scope = new Scope
+      exit: (path, {scope}) ->
+        addVariableDeclarations(path, scope) if scope.hasDeclaredVariables()
+    Identifier: ({node: {declaration, name}}, {scope}) ->
+      scope.addDeclaredVariable name if declaration
   )
 
 module.exports = transformer
